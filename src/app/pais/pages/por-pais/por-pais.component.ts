27c3ccf3f65ff7a7d19cccd87f1657477c1f7397
dcortaberria <david.cortaberria@entelgy-ibai.com>
2022-07-26 12:50:35 +0200

    Finishing the project

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,19 +4,29 @@ import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles:[
+    `
+      li{
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = "";
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService:PaisService) { }
 
-  buscar(){
+  buscar(termino:string){
     this.hayError=false;
-    console.log(this.termino);
+    this.termino=termino;
+    
     this.paisService.buscarPais(this.termino)
       .subscribe({
         next:(paises)=>{
@@ -28,6 +38,27 @@ export class PorPaisComponent {
           this.paises=[];
         }
       })
+  }
+
+  sugerencias(termino:string){
+    this.mostrarSugerencias=true;
+    this.hayError=false;
+    this.termino=termino;
+
+    this.paisService.buscarPais(termino)
+    .subscribe({
+      next:(paises)=>{
+        this.paisesSugeridos=paises.splice(0,5);
+      },
+      error:(err)=>{
+        this.hayError=true;
+        this.paises=[];
+      }
+    })
+  }
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -10,11 +10,30 @@ export class PaisService {
 
   private apiBaseURL: string = "https://restcountries.com/v2";
 
+  get getHttpParams(){
+    return new HttpParams().set("fields","name,capital,alpha2Code,flag,population");
+  }
+
   constructor(private http:HttpClient) { }
 
   buscarPais(termino:string):Observable<Country[]>{
     const url = `${this.apiBaseURL}/name/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params: this.getHttpParams});
+  }
+
+  buscarPaisPorCapital(termino:string):Observable<Country[]>{
+    const url = `${this.apiBaseURL}/capital/${termino}`;
+    return this.http.get<Country[]>(url,{params: this.getHttpParams});
+  }
+
+  buscarPaisPorAlpha(id:string):Observable<Country>{
+    const url = `${this.apiBaseURL}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
+  
+  buscarPaisesPorRegion(region:string):Observable<Country[]>{
+    const url = `${this.apiBaseURL}/region/${region}`;
+    return this.http.get<Country[]>(url,{params: this.getHttpParams});
   }
 
 }
